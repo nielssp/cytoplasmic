89bc93120cf19172b69c08e9b563ed4ef1aff584
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2021-05-23 11:08:05 +0200

    Add checkbox and radio controls.

--- src/form.tsx
+++ src/form.tsx
@@ -11,14 +11,81 @@ export function createId(prefix: string): string {
     return id;
 }
 
-export class TextControl extends Property<string> {
-    private inputs: HTMLElement[] = [];
+export abstract class Control<T> extends Property<T> {
     readonly disabled = new Property(false);
     constructor(
-        value: string,
+        value: T,
         public readonly id: string = createId('control'),
     ) {
         super(value);
+    }
+
+    abstract add(element: HTMLElement): void;
+}
+
+export class CheckboxControl extends Control<boolean> {
+    private inputs: HTMLElement[] = [];
+    constructor(
+        value: boolean,
+        id?: string,
+    ) {
+        super(value, id);
+    }
+
+    add(element: HTMLElement) {
+        switch (element.tagName) {
+            case 'LABEL':
+                (element as HTMLLabelElement).htmlFor = this.id;
+                break;
+            case 'INPUT':
+                this.addCheckboxInput(element as HTMLInputElement);
+                break;
+        }
+    }
+
+    protected addCheckboxInput(input: HTMLInputElement) {
+        if (!this.inputs.length) {
+            input.id = this.id;
+        }
+        this.inputs.push(input);
+        this.getAndObserve(v => input.checked = v);
+        input.addEventListener('change', () => {
+            this.value = input.checked;
+        });
+        this.disabled.getAndObserve(disabled => input.disabled = disabled);
+    }
+
+    focus() {
+        this.inputs.length && this.inputs[0].focus();
+    }
+
+    blur() {
+        this.inputs.length && this.inputs[0].focus();
+    }
+}
+
+export class RadioControl extends CheckboxControl {
+    constructor(
+        value: boolean,
+        public readonly name: string,
+        id?: string,
+    ) {
+        super(value, id);
+    }
+
+    protected addCheckboxInput(input: HTMLInputElement) {
+        super.addCheckboxInput(input);
+        input.name = this.name;
+    }
+}
+
+export class TextControl extends Control<string> {
+    private inputs: HTMLElement[] = [];
+    constructor(
+        value: string,
+        id?: string,
+    ) {
+        super(value, id);
     }
 
     add(element: HTMLElement) {
@@ -36,8 +103,10 @@ export class TextControl extends Property<string> {
     }
 
     private addTextInput(input: HTMLInputElement|HTMLTextAreaElement) {
+        if (!this.inputs.length) {
+            input.id = this.id;
+        }
         this.inputs.push(input);
-        input.id = this.id;
         this.getAndObserve(v => input.value = v);
         let interval: number|undefined;
         input.addEventListener('focus', () => {
@@ -68,12 +137,36 @@ export class TextControl extends Property<string> {
     }
 }
 
+export class RadioGroup<T extends string|number|symbol> extends Property<T> {
+    readonly disabled = new Property(false);
+    readonly radios = {} as Record<T, RadioControl>;
+    constructor(
+        value: T,
+        public readonly name: string = createId('radiogroup'),
+    ) {
+        super(value);
+    }
+
+    get(value: T): RadioControl {
+        if (!this.radios.hasOwnProperty(value)) {
+            this.radios[value] = new RadioControl(this.value === value, this.name);
+            this.radios[value].observe(checked => {
+                if (checked) {
+                    this.value = value;
+                }
+            });
+            this.disabled.getAndObserve(disabled => this.radios[value].disabled.value = disabled);
+        }
+        return this.radios[value];
+    }
+}
+
 export function Field(props: {
     children: JSX.Element|JSX.Element[],
     value: Property<string>|string,
 } | {
     children: JSX.Element|JSX.Element[],
-    control: TextControl,
+    control: Control<any>,
 }) {
     const children = flatten(props.children);
     const control = 'control' in props ? props.control : new TextControl('');
